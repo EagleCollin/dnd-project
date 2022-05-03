919ca6fec0a5edfa6ddfad0452351491496f6295
EagleCollin <scoutcollin97@gmail.com>
2022-05-03 16:01:20 -0500

    Setting up project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CharacterHomeComponent } from './character-home/character-home.component';
+import { CharacterListComponent } from './character-list/character-list.component';
+import { CharacterComponent } from './character/character.component';
+import { CharacterDisplayComponent } from './character-display/character-display.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterHomeComponent,
+    CharacterListComponent,
+    CharacterComponent,
+    CharacterDisplayComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
