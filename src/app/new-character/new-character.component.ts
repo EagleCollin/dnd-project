80f409456a31962c3ed672c672bed47e1a63c14c
EagleCollin <scoutcollin97@gmail.com>
2022-05-04 18:31:50 -0500

    added new character form
    I still need to add functionality based on the API but will look when I get home later

--- src/app/new-character/new-character.component.ts
+++ src/app/new-character/new-character.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-character',
   templateUrl: './new-character.component.html',
-  styleUrls: ['./new-character.component.css']
+  styleUrls: ['./new-character.component.css'],
 })
 export class NewCharacterComponent implements OnInit {
+  reactiveForm: FormGroup;
+  formSubmitted: boolean = false;
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onFormSubmit() {
+    this.formSubmitted = true;
+
+    this.reactiveForm.reset();
   }
 
+  onResetForm() {
+    this.router.navigate(['../'], { relativeTo: this.route });
+  }
 }
