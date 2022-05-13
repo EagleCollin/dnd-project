b6e6b4e5fb388fd51b59bf9b4a1d0c74130d92cf
EagleCollin <scoutcollin97@gmail.com>
2022-05-12 21:34:15 -0500

    Added proficiency form displays

--- src/app/new-character/proficiency-selection/proficiency-selection.component.ts
+++ src/app/new-character/proficiency-selection/proficiency-selection.component.ts
@@ -11,18 +11,18 @@ import {
   styleUrls: ['./proficiency-selection.component.css'],
 })
 export class ProficiencySelectionComponent implements OnInit {
-  proficiencyChoices = [];
-  proficiencyChoiceTypes= [];
+  proficiencyChoiceTypes = [];
   proficiencyChoiceNumber = [];
 
   constructor(private newCharacterService: NewCharacterService) {}
 
   ngOnInit(): void {
     this.newCharacterService.proficiencyChoices.subscribe((response) => {
-      this.proficiencyChoiceNumber = response.map((res)=> res.choose);
+      this.proficiencyChoiceNumber = response.map((res) => res.choose);
       this.proficiencyChoiceTypes = response.map((data) => data.from);
 
       console.log(this.proficiencyChoiceTypes);
+      console.log(this.proficiencyChoiceNumber);
     });
   }
 }
